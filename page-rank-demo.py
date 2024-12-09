1e3bbc3cbb5f209fdad25f29548b117f1ab3a902
abhagavathi <amol-bhagavathi@uiowa.edu>
2024-12-09 15:40:07 -0600

    adding comments for readability

--- page-rank-demo.py
+++ page-rank-demo.py
@@ -1,22 +1,28 @@
 import numpy as np
 
 def page_ranking(adjaceny_matrix, d=0.85, tolerance=1e-6):
-    col_sums = adjaceny_matrix.sum(axis=0)
-    adjaceny_matrix[:, col_sums == 0] = 1 / adjaceny_matrix.shape[0]
+    # getting the number of "websites"
     N = np.shape(adjaceny_matrix)[0]
+
+    # initializing pagerank for each website
     w = np.ones(N) / N
+
+    # initializing teleportation vector
     teleport = np.ones(N) / N
     v = d * np.dot(adjaceny_matrix, w) + (1 - d) * teleport
+
+    # iterating until convergence
     while np.linalg.norm(v - w) > tolerance:
         w = v
+        # using the given pagerank formula
         v = d * np.dot(adjaceny_matrix, w) + (1 - d) * teleport
     return v
 
 # example
 adjaceny_matrix = np.array([
-    [0,   0,   0,   0  ],
-    [0,   0,   0,   0  ],
-    [1, 0.5,   0,   0  ],
+    [0,   0,   0,   0.25  ],
+    [0,   0,   0,   0.25  ],
+    [1, 0.5,   0,   0.5  ],
     [0, 0.5,   1,   0  ]
 ])
 
